96ab776089c8f1ad1c0b4df85828fc821b5f285b
David Dong <daviddong201712@gmail.com>
2021-02-01 13:18:22 -0600

    4

---
+++ class_Circle.php
@@ -0,0 +1,17 @@
+<?php
+class Circle implements Shape
+{
+    private $radius;
+    function __construct($sides = "")
+    {
+        $this->radius = $sides["radius"];
+    }
+    function area()
+    {
+        return pi()*$this->radius * $this->radius;
+    }
+    function perimeter()
+    {
+        return 2 * pi()*$this->radius;
+    }
+}
---
+++ class_Triangle.php
@@ -0,0 +1,22 @@
+<?php
+class Triangle implements Shape
+{
+    private $side1;
+    private $side2;
+    private $side3;
+    function __construct($sides = "")
+    {
+        $this->side1 = $sides["side1"];
+        $this->side2 = $sides["side2"];
+        $this->side3 = $sides["side3"];
+    }
+    function area()
+    {
+        $s = ($this->side1 + $this->side2 + $this->side3) / 2;
+        return sqrt($s * ($s - $this->side1) * ($s - $this->side1) * ($s - $this->side1));
+    }
+    function perimeter()
+    {
+        return $this->side1 + $this->side2 + $this->side3;
+    }
+}
